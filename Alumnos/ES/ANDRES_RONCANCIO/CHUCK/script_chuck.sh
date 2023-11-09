5680900cd1afbdb25e4c8b127ace8fbd2204f579
Gandrex87 <andresrsalamanca@gmail.com>
2023-11-09 19:29:30 +0100

    Draft script en clase
    Ejercicio en clase

---
+++ Alumnos/ES/ANDRES_RONCANCIO/CHUCK/script_chuck.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd 
+echo "Hola mundo"
+#mkdir CHUCK && mkdir CHUCK/resultados
+
+python /Users/andresrsalamanca/Documents/GitHub/EDEM_MDA2324/Alumnos/ES/ANDRES_RONCANCIO/CHUCK/resultado.py
+
+
